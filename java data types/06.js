eb08aeaed591a4db56023b55bf046efc248822bc
ZemfiraAlakbar <127456372+ZemfiraAlakbar@users.noreply.github.com>
2023-04-11 21:30:36 -0700

    Add files via upload

---
+++ java data types/06.js
@@ -0,0 +1,12 @@
+let text = "there";
+console.log(`hello ${3+8}`);
+console.log(`hello ${`3+8`}`);
+console.log(`hello ${"text"}`);
+console.log(`hello ${text}`);
+let firstName = "Zenfira"
+let lastName = "alakbarova"
+let age =   20
+let city = "ganja"
+let uni ="Gdu"
+let info =  `${firstName} ${lastName} ${age} ${city} ${uni}`
+console.log(`${info}`);
